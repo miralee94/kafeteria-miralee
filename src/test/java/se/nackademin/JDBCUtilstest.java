4e0c922b944f83bbb58b28a35a5f17862ff84ae2
Mira Lee <leemira1994@gmail.com>
2023-02-26 14:53:30 +0100

    Added javadoc for tests

--- src/test/java/se/nackademin/JDBCUtilstest.java
+++ src/test/java/se/nackademin/JDBCUtilstest.java
@@ -4,9 +4,14 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 import org.junit.Test;
 import se.nackademin.Beverages.DrinkSizes;
-
+/**
+* The JDBCUtilsTest class is used to test the JDBCUtils class, which contaiins methods to intetact with a MySQL database.
+*/
 public class JDBCUtilsTest {
-
+    /**
+     * Creates a JDBCUtils object with the given IP address and port number to connect to the MySQL database.
+     * Tests the default menu of CoffeeDrink objects by connecting to the database and calling the listBeverageMenuDetails() method from JDBUtils class.
+     */
     JDBCUtils JDBCUtilsObject = new JDBCUtils("127.0.0.1", "3306");
     @Test
     public void checkDefaultMenu() {
@@ -34,18 +39,26 @@ public class JDBCUtilsTest {
         assertEquals(listDrinkDetails.get(1).getDrinkPrice(),45.00, 0);
 
     }
-    //Making an order and check my list orders
+
+    /**
+     * Tests the behavior of the place order method in the system.
+     * Connect to the databse using the JDBUtilsObject.
+     * Inserts a new order for a small coffee with the title "Coffee" and the price of 35.00.
+     * Retrieves the list of orders using the JDBUtilsObject.
+     * Checks that the price of the last (i.e., most recent) order in the list is 35.00.
+     * Checks that the title of the last order in the list is "Coffee".
+     * Checks that the size of the last order is small.
+     */
     @Test
     public void checkPlaceOrder() {
         JDBCUtilsObject.connectToDatabase();
-        JDBCUtilsObject.insertOrder(new CoffeeDrink("Coffee", DrinkSizes.MEDIUM, 45.00));
+        JDBCUtilsObject.insertOrder(new CoffeeDrink("Coffee", DrinkSizes.SMALL, 35.00));
         List<CoffeeDrink>orders = JDBCUtilsObject.listOrders();
-        //Cheking last (12th in my case) drinks price
-        assertEquals(orders.get(18).getDrinkPrice(), 45.00, 0);
 
-        //Cheking last drinks title
-        assertEquals(orders.get(18).getDrinkTitle(), "Coffee");
+        assertEquals(orders.get(orders.size() -1).getDrinkPrice(), 35.00, 0);
 
-        //Cheking last drinks size
-        assertEquals(orders.get(18).getDrinkSize(), DrinkSizes.MEDIUM);
-}}
+        assertEquals(orders.get(orders.size() -1).getDrinkTitle(), "Coffee");
+
+        assertEquals(orders.get(orders.size() -1).getDrinkSize(), DrinkSizes.SMALL);
+}
+}
